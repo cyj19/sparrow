67dd2c8ea102341f5db36d3edf66618c1d7fb497
chenyuji <201998@gree.com.cn>
2022-03-01 16:28:39 +0800

    modify protocol

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -9,58 +9,49 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
-	"log"
 )
 
 // 消息协议设计 使用前缀长度法
 /**
 Header:
-| start | version | codecType | magicSize | serviceNameSize | serviceMethodSize | metaDataSize | payloadSize |
-| 0x03  |   0x01  |     1     |     4     |         4       |          4        |       4      |      4      |
+| start | version | codecType | compressionType | magicSize | serviceNameSize | serviceMethodSize | payloadSize |
+| 0x03  |   0x01  |     1     |        1        |     4     |         4       |          4        |      4      |
 
 Body:
-| magic | serviceName | serviceMethod | metaData | payload |
-|   x   |     x       |       x       |    x     |     x   |
+| magic | serviceName | serviceMethod | payload |
+|   x   |     x       |       x       |    x    |
 
 */
 
 const (
-	HeaderSize = 23
+	HeaderSize = 20
 	StartChar  = byte(3)
 )
 
 // Header 定义消息头
 type Header struct {
-	Start             byte
-	Version           byte
-	CodecType         byte
-	MagicSize         uint32
-	ServiceNameSize   uint32
-	ServiceMethodSize uint32
-	MetaDataSize      uint32
-	PayLoadSize       uint32
+	Start             byte   // 起始符
+	Version           byte   // 版本号
+	CodecType         byte   // 序列化类型
+	CompressionType   byte   // 压缩类型
+	MagicSize         uint32 // 魔法值大小
+	ServiceNameSize   uint32 // 服务名称大小
+	ServiceMethodSize uint32 // 服务方法大小
+	PayLoadSize       uint32 // 函数参数大小
 }
 
 // Body 定义消息体
 type Body struct {
-	Magic         string // 大小不确定，用string
-	ServiceName   string
-	ServiceMethod string
-	MetaData      []byte
-	Payload       []byte
+	Magic         string // 魔法值
+	ServiceName   string // 服务名称
+	ServiceMethod string // 服务方法
+	Payload       []byte // 函数参数
 }
 
 // Message 定义消息
 type Message struct {
 	Header *Header
 	Body   *Body
-}
-
-type Protocol struct {
-}
-
-func NewProtocol() *Protocol {
-	return &Protocol{}
 }
 
 // DecodeMessage 解码消息
@@ -87,7 +78,7 @@ func DecodeMessage(r io.Reader) (*Message, error) {
 	if err != nil {
 		return nil, err
 	}
-	bodySize := header.MagicSize + header.ServiceNameSize + header.ServiceMethodSize + header.MetaDataSize + header.PayLoadSize
+	bodySize := header.MagicSize + header.ServiceNameSize + header.ServiceMethodSize + header.PayLoadSize
 	bodyData := make([]byte, bodySize)
 	// 读取消息体的数据
 	_, err = io.ReadFull(r, bodyData)
@@ -109,16 +100,16 @@ func DecodeMessage(r io.Reader) (*Message, error) {
 
 func DecodeHeader(data []byte) (*Header, error) {
 	header := &Header{
-		Start:     data[0],
-		Version:   data[1],
-		CodecType: data[2],
+		Start:           data[0],
+		Version:         data[1],
+		CodecType:       data[2],
+		CompressionType: data[3],
 	}
 	// 大端字符序转为uint32
-	header.MagicSize = binary.BigEndian.Uint32(data[2:6])
-	header.ServiceNameSize = binary.BigEndian.Uint32(data[6:10])
-	header.ServiceMethodSize = binary.BigEndian.Uint32(data[10:14])
-	header.MetaDataSize = binary.BigEndian.Uint32(data[14:18])
-	header.PayLoadSize = binary.BigEndian.Uint32(data[18:22])
+	header.MagicSize = binary.BigEndian.Uint32(data[4:8])
+	header.ServiceNameSize = binary.BigEndian.Uint32(data[8:12])
+	header.ServiceMethodSize = binary.BigEndian.Uint32(data[12:16])
+	header.PayLoadSize = binary.BigEndian.Uint32(data[16:20])
 	return header, nil
 }
 
@@ -128,9 +119,7 @@ func DecodeBody(data []byte, header *Header) (*Body, error) {
 	magicSize := header.MagicSize
 	serviceNameSize := header.ServiceNameSize
 	serviceMethodSize := header.ServiceMethodSize
-	metaDataSize := header.MetaDataSize
 	payloadSize := header.PayLoadSize
-	log.Println(magicSize, serviceNameSize, serviceMethodSize, metaDataSize, payloadSize)
 
 	var startIndex uint32 = 0
 	endIndex := startIndex + magicSize
@@ -157,13 +146,6 @@ func DecodeBody(data []byte, header *Header) (*Body, error) {
 	body.ServiceMethod = string(serviceMethod)
 
 	startIndex = endIndex
-	endIndex = startIndex + metaDataSize
-	length = endIndex - startIndex
-	metaData := make([]byte, length)
-	copy(metaData, data[startIndex:endIndex])
-	body.MetaData = metaData
-
-	startIndex = endIndex
 	endIndex = startIndex + payloadSize
 	length = endIndex - startIndex
 	payload := make([]byte, length)
@@ -179,18 +161,18 @@ func EncodeMessage(message *Message) ([]byte, error) {
 	serviceNameByte := []byte(body.ServiceName)
 	serviceMethodByte := []byte(body.ServiceMethod)
 
-	msgSize := HeaderSize + len(body.Magic) + len(serviceNameByte) + len(serviceMethodByte) + len(body.MetaData) + len(body.Payload)
+	msgSize := HeaderSize + len(body.Magic) + len(serviceNameByte) + len(serviceMethodByte) + len(body.Payload)
 	data := make([]byte, msgSize)
 
 	// 构建头部
 	data[0] = header.Start
 	data[1] = header.Version
 	data[2] = header.CodecType
-	binary.BigEndian.PutUint32(data[2:6], uint32(len(body.Magic)))
-	binary.BigEndian.PutUint32(data[6:10], uint32(len(serviceNameByte)))
-	binary.BigEndian.PutUint32(data[10:14], uint32(len(serviceMethodByte)))
-	binary.BigEndian.PutUint32(data[14:18], uint32(len(body.MetaData)))
-	binary.BigEndian.PutUint32(data[18:22], uint32(len(body.Payload)))
+	data[3] = header.CompressionType
+	binary.BigEndian.PutUint32(data[4:8], uint32(len(body.Magic)))
+	binary.BigEndian.PutUint32(data[8:12], uint32(len(serviceNameByte)))
+	binary.BigEndian.PutUint32(data[12:16], uint32(len(serviceMethodByte)))
+	binary.BigEndian.PutUint32(data[16:20], uint32(len(body.Payload)))
 
 	// 构建body
 	startIndex := HeaderSize
@@ -204,10 +186,6 @@ func EncodeMessage(message *Message) ([]byte, error) {
 	startIndex = endIndex
 	endIndex = startIndex + len(body.ServiceMethod)
 	copy(data[startIndex:endIndex], body.ServiceMethod)
-
-	startIndex = endIndex
-	endIndex = startIndex + len(body.MetaData)
-	copy(data[startIndex:endIndex], body.MetaData)
 
 	startIndex = endIndex
 	endIndex = startIndex + len(body.Payload)
